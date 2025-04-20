eb700efa81ed5930cfae06c2171ca3d7734e836f
marcophilip <marcophilipagapito@gmail.com>
2025-04-19 17:42:00 -0700

    create functionality completed along with with form actions and route built

--- controllers/foods.js
+++ controllers/foods.js
@@ -8,8 +8,18 @@ const User = require('../models/user.js');
 // router logic will go here - will be built later on in the lab
 
 // GET /users/:userId/foods
-router.get('/', (req, res) => {
-    res.render('foods/index.ejs');
+router.get('/', async (req, res) => {
+    try {
+        // look up the user from session
+        const currentUser = await User.findById(req.session.user._id);
+        // render the foods/index.ejs
+        res.render('foods/index.ejs', {
+            pantry: currentUser.pantry,
+        });
+    } catch (error) {
+        // if any errors redirect back home
+        res.redirect('/');
+    }
 });
 
 // GET /users/:userId/foods/new
@@ -17,5 +27,24 @@ router.get('/new', (req, res) => {
     res.render('foods/new.ejs');
 });
 
+// GET /users/:userId/foods
+
+
+// POST /users/:userId/foods
+router.post('/', async (req, res) => {
+    try {
+        // look up the user from DB
+        const currentUser = await User.findById(req.session.user._id);
+        // push req.body to the pantry array of the current user
+        currentUser.pantry.push(req.body);
+        // save changes to the user
+        await currentUser.save();
+        // redirect user back to the foods index view
+        res.redirect(`/users/${currentUser._id}/foods`);
+    } catch (error) {
+        // if error occurs, log them and redirect back home
+        res.redirect('/');
+    }
+});
 
 module.exports = router;
